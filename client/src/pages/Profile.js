a8a1cace34d96ee462f8f26fedb86425a4da75b5
bptiago <125921557+bptiago@users.noreply.github.com>
2024-02-11 11:31:55 -0300

    Remove user made post

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,12 +4,14 @@ import "../components/Form.css";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import axios from "axios";
 import { AuthContext } from "../helpers/AuthContext";
+import Popup from "../components/Popup";
 
 function Profile() {
   const { auth } = useContext(AuthContext);
 
   const [userDob, setUserDob] = useState("");
   const [userPosts, setUserPosts] = useState([]);
+  const [popup, setPopup] = useState({ show: false, postId: 0 });
 
   const userId = auth.id;
   const username = auth.username;
@@ -25,6 +27,19 @@ function Profile() {
         setUserPosts(response.data);
       });
   }, []);
+
+  const removePost = (postId) => {
+    axios
+      .delete(`http://localhost:8080/posts/delete/${postId}`)
+      .then(() => {
+        const updatedPostList = userPosts.filter((post) => post.id !== postId);
+        setUserPosts(updatedPostList);
+        console.log("removed");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div style={{ marginTop: "10vh" }}>
@@ -50,6 +65,7 @@ function Profile() {
               <div
                 className="container"
                 style={{ justifyContent: "space-between" }}
+                key={post.id}
               >
                 <div className="post-info">
                   <h3>{post.title}</h3>
@@ -59,15 +75,41 @@ function Profile() {
                     <p>{post.postText}</p>
                   )}
                 </div>
-                <div className="post-btns">
-                  <button style={{ marginRight: "10px" }}>Edit</button>
-                  <button>Remove</button>
+                <div style={{ paddingRight: "30px" }}>
+                  <button
+                    className="profile-post-btn"
+                    onClick={() => {
+                      setPopup({ show: true, postId: post.id });
+                    }}
+                  >
+                    Remove
+                  </button>
                 </div>
               </div>
             );
           })
         )}
       </div>
+      <Popup trigger={popup.show}>
+        <h4>Are you sure you wish to delete this post?</h4>
+        <button
+          className="yes-btn"
+          onClick={() => {
+            removePost(popup.postId);
+            setPopup({ show: false, postId: 0 });
+          }}
+        >
+          YES
+        </button>
+        <button
+          className="no-btn"
+          onClick={() => {
+            setPopup({ show: false, postId: 0 });
+          }}
+        >
+          NO
+        </button>
+      </Popup>
     </div>
   );
 }
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const { Posts, Likes } = require("../models");
 const { validateToken } = require("../middlewares/Auth");
 
+// Create post
 router.post("/", async (req, res) => {
   const post = req.body;
   await Posts.create(post);
   return res.json("Post created");
 });
 
+// Get all posts and user liked posts
 router.get("/getAll", validateToken, async (req, res) => {
   const userId = req.user.id;
   const likedPosts = await Likes.findAll({
@@ -23,6 +25,7 @@ router.get("/getAll", validateToken, async (req, res) => {
   return res.json({ posts, likedPosts });
 });
 
+// Get user made posts
 router.get("/user/:username", async (req, res) => {
   const username = req.params.username;
 
@@ -33,6 +36,25 @@ router.get("/user/:username", async (req, res) => {
   });
 
   return res.json(posts);
+});
+
+// Delete user post by id
+router.delete("/delete/:id", async (req, res) => {
+  const postId = req.params.id;
+
+  await Likes.destroy({
+    where: {
+      PostId: postId,
+    },
+  });
+
+  await Posts.destroy({
+    where: {
+      id: postId,
+    },
+  });
+
+  res.json("post and likes removed");
 });
 
 module.exports = router;
